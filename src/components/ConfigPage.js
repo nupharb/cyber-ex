a165da8cabeaed93acd562071e62fb13df31b420
nuphar <nupharmor@hotmail.com>
2018-10-25 11:35:53 +0300

    adding the speficic layout to the configuration page

--- src/components/ConfigPage.js
+++ src/components/ConfigPage.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
-import { layouts } from '../constants/data'
+import { layouts } from '../constants/data';
+import RGL, { WidthProvider } from 'react-grid-layout';
+
+const ReactGridLayout = WidthProvider(RGL);
 
 const ConfigPage = ({ match }) => {
-    console.log(match);
-    return <div>configuration</div>;
+  const gridLayout = layouts[match.params.id].gridLayout;
+  console.log(match, gridLayout);
+  return (
+    <div className='config-page'>
+      <ReactGridLayout 
+        className='grid-layout'
+        layout={ gridLayout.layout } 
+        cols={ gridLayout.cols } 
+        rowHeight={ gridLayout.rowHeight }
+      >
+        { gridLayout.layout.map((placeholder, index) => (
+          <div key={ placeholder.i }>{ placeholder.i }</div>
+        )) }
+        
+      </ReactGridLayout>
+    </div>
+  );
 }
     
 
--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import RGL, { WidthProvider } from 'react-grid-layout';
 
 const ReactGridLayout = WidthProvider(RGL);
